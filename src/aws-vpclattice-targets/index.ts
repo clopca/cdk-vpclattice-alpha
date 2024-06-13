e8798abd7574f2c649a746200dc0b06a45b84248
clopca <clopca@amazon.es>
2024-06-13 14:05:46 +0200

    cleaned repo to start from scratch

--- src/aws-vpclattice-targets/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './target';
-export * from './healthcheck';
-export * from './alb';
-export * from './lambda';
-export * from './ec2';
-export * from './ip';
--- test/hello.test.ts
+++
@@ -1,4 +0,0 @@
-
-test('hello', () => {
-  console.log('hello');
-});
