b8b561189e3a4ec89d961e413b85fc3c8243df1e
clopca <clopca@amazon.es>
2024-06-14 20:33:54 +0200

    updated cdk to 2.146 and added integration folder for testing

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,7 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'clopca',
   authorAddress: 'clopca@amazon.es',
-  cdkVersion: '2.145.0',
+  cdkVersion: '2.146.0',
   keywords: ['aws-cdk', 'cdk', 'vpclattice'],
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.4.0',
@@ -14,11 +14,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
   docgen: true,
   devDeps: [
     'ts-node',
-    'aws-cdk-lib@2.145.0', // Exact version as dev dependency
+    'aws-cdk-lib@2.146.0', // Exact version as dev dependency
     'constructs@10.0.5', // Exact version as dev dependency
   ],
-  peerDeps: ['aws-cdk-lib@^2.145.0', 'constructs@^10.0.5'],
-  // deps: ['aws-cdk-lib@^2.145.0', 'constructs@^10.0.5'],
+  peerDeps: ['aws-cdk-lib@^2.146.0', 'constructs@^10.0.5'],
+  // deps: ['aws-cdk-lib@^2.146.0', 'constructs@^10.0.5'],
   description: 'A CDK L2 Construct Library for VPCLattice',
   npmProvenance: false,
 });
---
+++ src/integration/lib/vpclatticealpha-stack.ts
@@ -0,0 +1,82 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as vpclattice from '../..';
+// import * as targets from '../../aws-vpclattice-targets';
+// import { SupportResources } from './support';
+// import { aws_iam as iam } from 'aws-cdk-lib';
+
+export class VpclatticealphaStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // const support = new SupportResources(this, 'SupportResources');
+
+    // create a vpc lattice service, and associate it with the service network
+    // the listener use defaults of HTTPS, on port 443, and have a default action of 404 NOT FOUND
+    new vpclattice.Service(this, 'myLatticeService', {
+      // we will all unauthenticated requests to be used
+    });
+
+    // // add a listener to the service
+    // const listener = new vpclattice.Listener(this, 'Listener', {
+    //   service: myLatticeService,
+    // });
+
+    // // add a listenerRule that will use the helloworld lambda as a Target
+    // listener.addListenerRule({
+    //   name: 'helloworld',
+    //   priority: 10,
+    //   action: [
+    //     {
+    //       targetGroup: new vpclattice.TargetGroup(this, 'hellolambdatargets', {
+    //         name: 'hellowworld',
+    //         target: new targets.LambdaTarget(this, 'helloworld', { lambda: [support.helloWorld] }),
+    //         // vpclattice.Target.lambda([support.helloWorld]),
+    //       }),
+    //     },
+    //   ],
+
+    //   httpMatch: {
+    //     pathMatches: { path: '/hello' },
+    //   },
+    //   // we will only allow access to this service from the ec2 instance
+    //   accessMode: vpclattice.RuleAccessMode.UNAUTHENTICATED,
+    // });
+
+    // //add a listenerRule that will use the goodbyeworld lambda as a Target
+    // listener.addListenerRule({
+    //   name: 'goodbyeworld',
+    //   priority: 20,
+    //   action: [
+    //     {
+    //       targetGroup: new vpclattice.TargetGroup(this, 'goodbyelambdatargets', {
+    //         name: 'goodbyeworld',
+    //         target: new targets.LambdaTarget(this, 'goodbyeworld', { lambda: [support.goodbyeWorld] }),
+    //       }),
+    //     },
+    //   ],
+
+    //   httpMatch: {
+    //     pathMatches: { path: '/goodbye' },
+    //   },
+    //   // we will only allow access to this service from the ec2 instance
+    //   allowedPrincipals: [support.ec2instance.role],
+    //   accessMode: vpclattice.RuleAccessMode.AUTHENTICATED_ONLY,
+    // });
+
+    // // create a latticeServiceNetwork using the default settings for a Service network;
+    // // - Requires an IAM policy, do not allow access outside this org,
+    // // Overide the default option to allow unauthenticated/signed requests
+    // // associate the vpcs
+    // // assocaite the services with the servicenetwork
+    // const serviceNetwork = new vpclattice.ServiceNetwork(this, 'ServiceNetwork', {
+    //   accessmode: vpclattice.ServiceNetworkAccessMode.UNAUTHENTICATED,
+    //   vpcs: [support.vpc1],
+    //   services: [myLatticeService],
+    // });
+
+    // // after adding rules, apply the auth policy to the service and Service Network
+    // myLatticeService.applyAuthPolicy();
+    // serviceNetwork.applyAuthPolicyToServiceNetwork();
+  }
+}
