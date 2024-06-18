2d00249a6cde947dc41445d7afa11be2591eb5be
Rafael Mosca <rafams@amazon.com>
2024-06-18 19:00:01 +0200

    adding targets

---
+++ src/aws-vpclattice-targets/alb-target.ts
@@ -0,0 +1,34 @@
+import { IApplicationLoadBalancer } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { Protocol } from "./target";
+import { IVpc } from "aws-cdk-lib/aws-ec2";
+
+export interface AlbTargetGroupProps {
+    /**
+     * The name of the target group
+     */
+    readonly name: string;
+
+    /**
+     * You can specify a single Application Load Balancer as the target.
+     */
+    readonly target: IApplicationLoadBalancer;
+
+    /**
+     * VPC Identifier
+     */
+    readonly vpcIdentifier: IVpc;
+
+    /**
+     * The protocol to use for routing traffic to the targets.
+     * Can't be changed after creation.
+     * @default Protocol.HTTPS
+     */
+    readonly protocol?: Protocol;
+
+    /**
+     * The port on which the target will listen.
+     * @default 443
+     */
+    readonly port?: number;
+
+}
---
+++ src/aws-vpclattice-targets/instance-target.ts
@@ -0,0 +1,8 @@
+
+export interface InstanceTargetGroupProps {
+	/**
+	 * The name of the target group
+	 */
+	readonly name: string;
+
+}
