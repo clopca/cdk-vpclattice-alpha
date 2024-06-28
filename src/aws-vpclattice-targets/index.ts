df68b652659995346912ad5ece61d51edf28f90d
clopca <clopca@amazon.com>
2024-06-28 21:01:41 +0200

    added missing rules import

--- src/aws-vpclattice-targets/index.ts
+++ src/aws-vpclattice-targets/index.ts
@@ -3,7 +3,6 @@ export * from './base-target-group';
 export * from './health-check';
 // Target types
 export * from './alb-target';
-export * from './lambda-target';
 export * from './instance-target';
 export * from './ip-target';
-
+export * from './lambda-target';
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 // AWS::VpcLattice CloudFormation Resources:
 //export * from './vpc-lattice.generated';
 
+export * from './listener';
+export * from './logging';
+export * from './matches';
+export * from './rules';
+export * from './service-network-association';
 export * from './service-network';
 export * from './service';
-export * from './listener';
-export * from './matches';
-export * from './logging';
 export * from './util';
-
-
