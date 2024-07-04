327e0922d4c0eb39265a815a76c538581f799fe8
clopca <clopca@amazon.com>
2024-07-04 10:21:46 +0200

    added ip target tests

--- test/target-groups/unit/ip-tg.test.ts
+++ test/target-groups/unit/ip-tg.test.ts
@@ -1,19 +1,107 @@
 import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
-import { IpTargetGroup, RequestProtocol } from '../../../src/aws-vpclattice-targets';
+import {
+  HealthCheckProtocol,
+  HealthCheckProtocolVersion,
+  IpAddressType,
+  IpTargetGroup,
+  RequestProtocol,
+  RequestProtocolVersion,
+  TargetType,
+} from '../../../src/aws-vpclattice-targets';
+import { HTTPFixedResponse } from '../../../src';
 
-describe('InstanceTG', () => {
-  test('DeniesInvalidProtocolVersionCombo', () => {
+describe('IP Target Group', () => {
+  let app: cdk.App;
+  let stack: cdk.Stack;
+  let vpc: Vpc;
+
+  beforeEach(() => {
     // GIVEN
-    const app = new cdk.App();
-    const stack = new cdk.Stack(app, 'TestStack');
-    const vpc = new Vpc(stack, 'VPC', {});
+    app = new cdk.App();
+    stack = new cdk.Stack(app, 'TestStack');
+    vpc = new Vpc(stack, 'VPC', {});
+  });
 
+  //default ip target group
+  test('Default IP Target Group', () => {
     // WHEN & THEN
     new IpTargetGroup(stack, 'IpTG', {
       vpc,
-      protocol: RequestProtocol.HTTPS,
     });
-    expect(() => app.synth()).toThrow('Invalid protocol version combination.');
+    // Check the target group has been created
+    Template.fromStack(stack).hasResource('AWS::VpcLattice::TargetGroup', {
+      Properties: {
+        Type: TargetType.IP,
+        Config: {
+          IpAddressType: IpAddressType.IPV4,
+          Protocol: RequestProtocol.HTTPS,
+        },
+      },
+    });
+  });
+
+  test('Add target', () => {
+    // WHEN & THEN
+    const ipTargetGroup = new IpTargetGroup(stack, 'IpTG', {
+      name: 'ip-tg',
+      ipAddressType: IpAddressType.IPV4,
+      vpc,
+      targets: [],
+    });
+
+    ipTargetGroup.addTarget({
+      ipAddress: '10.0.0.1',
+      port: 80,
+    });
+    // Check the target has been added
+    Template.fromStack(stack).hasResource('AWS::VpcLattice::TargetGroup', {
+      Properties: {
+        Name: 'ip-tg',
+        Type: TargetType.IP,
+        Config: {
+          IpAddressType: IpAddressType.IPV4,
+          Protocol: RequestProtocol.HTTPS,
+        },
+        Targets: [
+          {
+            Id: '10.0.0.1',
+            Port: 80,
+          },
+        ],
+      },
+    });
+  });
+  test('Add target with ipv6 and healthcheck', () => {
+    // WHEN & THEN
+    new IpTargetGroup(stack, 'IpTG', {
+      vpc,
+      ipAddressType: IpAddressType.IPV6,
+      protocol: RequestProtocol.HTTP,
+      protocolVersion: RequestProtocolVersion.HTTP2,
+      healthCheck: {
+        enabled: false,
+        healthCheckInterval: cdk.Duration.seconds(30),
+        healthCheckTimeout: cdk.Duration.seconds(5),
+        path: '/health',
+        protocol: HealthCheckProtocol.HTTP,
+        port: 80,
+        protocolVersion: HealthCheckProtocolVersion.HTTP2,
+        unhealthyThresholdCount: 2,
+        healthyThresholdCount: 5,
+        matchers: HTTPFixedResponse.OK,
+      },
+    });
+    // Check the target has been added
+    Template.fromStack(stack).hasResource('AWS::VpcLattice::TargetGroup', {
+      Properties: {
+        Type: TargetType.IP,
+        Config: {
+          IpAddressType: IpAddressType.IPV6,
+          Protocol: RequestProtocol.HTTP,
+        },
+      },
+    });
   });
 });
